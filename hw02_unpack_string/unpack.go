a1b5496b56249ada65b25ec5e030bad8a41821db
Aleksey Kukin <maddog_lex@mail.ru>
2021-03-15 17:51:26 -0400

    HW2 ADDITIONAL task is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,10 +12,13 @@ var ErrInvalidString = errors.New("invalid string")
 Функция осуществляет примитивную распаковку строки, содержащую повторяющиеся символы/руны примеры:
 "a4bc2d5e" => "aaaabccddddde"   "abcd" => "abcd"
 "3abc" => "" (некорректная строка)      "45" => "" (некорректная строка)        "aaa10b" => "" (некорректная строка)
-"aaa0b" => "aab"        "" => ""        "d\n5abc" => "d\n\n\n\n\nabc".
+"aaa0b" => "aab"        "" => ""        "d\n5abc" => "d\n\n\n\n\nabc"
+Дополнительно:
+"qwe\4\5" => "qwe45"	"qwe\45" => "qwe44444"	"qwe\\5a" => "qwe\\\\\a".
 */
 func Unpack(input string) (string, error) {
 	var cache int32
+	var quoted bool
 	var unpacked strings.Builder
 	// Запускаем перебор по строке
 	for k, v := range input {
@@ -28,15 +31,18 @@ func Unpack(input string) (string, error) {
 		// Если символ буква или управляющий символ выводим символ из кеша, либо вносим его в кеш если он пуст.
 		case (unicode.IsLetter(v) || unicode.IsControl(v)):
 			{
+				if quoted {
+					return "", ErrInvalidString
+				}
 				if cache != 0 {
 					unpacked.WriteRune(cache)
 				}
 				cache = v
 			}
 		// Если символ цифра (n) и перед ней есть буква в кеше, "распаковываем кеш" n раз.
-		case unicode.IsDigit(v):
+		case unicode.IsDigit(v) && !quoted:
 			{
-				if !unicode.IsDigit(cache) && cache != 0 {
+				if /*!unicode.IsDigit(cache) && */ cache != 0 {
 					for i := v - 48; i > 0; i-- {
 						unpacked.WriteRune(cache)
 					}
@@ -45,7 +51,24 @@ func Unpack(input string) (string, error) {
 					return "", ErrInvalidString
 				}
 			}
-
+		// Если цифра экранирована
+		case unicode.IsDigit(v) && quoted:
+			{
+				cache = v
+				quoted = false
+			}
+		// Обрабатываем экранирование через \ используя флаг quoted.
+		case v == 92:
+			{
+				if !quoted {
+					unpacked.WriteRune(cache)
+					quoted = true
+				} else {
+					cache = v
+					quoted = false
+				}
+			}
+		// Если встретили непонятный символ - выводим ошибку.
 		default:
 			{
 				return "", ErrInvalidString
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -22,10 +22,14 @@ func TestUnpack(t *testing.T) {
 		{input: "ПриRет3", expected: "ПриRеттт"},
 
 		// uncomment if task with asterisk completed
-		//		{input: `qwe\4\5`, expected: `qwe45`},
-		//		{input: `qwe\45`, expected: `qwe44444`},
-		//		{input: `qwe\\5`, expected: `qwe\\\\\`},
-		//		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		// Дополнительные тесты на правильный вывод.
+		{input: `ва\\\2`, expected: `ва\2`},
+		{input: `Ява\\\2К`, expected: `Ява\2К`},
+		{input: `Я3К`, expected: `ЯЯЯК`},
 	}
 
 	for _, tc := range tests {
@@ -39,7 +43,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b", "a-1fs", "ПриRет345"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "a-1fs", "ПриRет345", `qw\ne`, "Я34В", "©"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
