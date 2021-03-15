736ca984e0071ed7eafdcf0d7e3fb773221c3449
Aleksey Kukin <maddog_lex@mail.ru>
2021-03-15 13:51:31 -0400

    HW2 BASE task is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,59 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+/*
+Функция осуществляет примитивную распаковку строки, содержащую повторяющиеся символы/руны примеры:
+"a4bc2d5e" => "aaaabccddddde"   "abcd" => "abcd"
+"3abc" => "" (некорректная строка)      "45" => "" (некорректная строка)        "aaa10b" => "" (некорректная строка)
+"aaa0b" => "aab"        "" => ""        "d\n5abc" => "d\n\n\n\n\nabc".
+*/
+func Unpack(input string) (string, error) {
+	var cache int32
+	var unpacked strings.Builder
+	// Запускаем перебор по строке
+	for k, v := range input {
+		switch {
+		// Если первый символ цифра - выдаем ошибку.
+		case (k == 0 && unicode.IsDigit(v)):
+			{
+				return "", ErrInvalidString
+			}
+		// Если символ буква или управляющий символ выводим символ из кеша, либо вносим его в кеш если он пуст.
+		case (unicode.IsLetter(v) || unicode.IsControl(v)):
+			{
+				if cache != 0 {
+					unpacked.WriteRune(cache)
+				}
+				cache = v
+			}
+		// Если символ цифра (n) и перед ней есть буква в кеше, "распаковываем кеш" n раз.
+		case unicode.IsDigit(v):
+			{
+				if !unicode.IsDigit(cache) && cache != 0 {
+					for i := v - 48; i > 0; i-- {
+						unpacked.WriteRune(cache)
+					}
+					cache = 0
+				} else {
+					return "", ErrInvalidString
+				}
+			}
+
+		default:
+			{
+				return "", ErrInvalidString
+			}
+		}
+	}
+	// Если в кеше остался последний символ выводим его.
+	if cache != 0 {
+		unpacked.WriteRune(cache)
+	}
+	return unpacked.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,11 +16,16 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		// Дополнительные тесты на правильный вывод.
+		{input: "ыа0в", expected: "ыв"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "ПриRет3", expected: "ПриRеттт"},
+
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		//		{input: `qwe\4\5`, expected: `qwe45`},
+		//		{input: `qwe\45`, expected: `qwe44444`},
+		//		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		//		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +39,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "a-1fs", "ПриRет345"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
